30ade7a339099076d17ef5560f3ea745abbe6460
DAR-LAB-MA\UCSE <a>
2022-05-27 21:20:11 -0300

    No se que hice

---
+++ JuegoDados/Interfaz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Interfaz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" BIENVENIDO AL JUEGO DE LA PAPA");
+            Console.WriteLine(" PUNTOS: ");
+
+            Servicio.ServiceClient servicio = new Servicio.ServiceClient();
+
+            servicio.ObtenerNumeros(new Random());
+        }
+    }
+}
---
+++ JuegoDados/ServicioWCF/App_Code/Service.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+	public string GetData(int value)
+	{
+		return string.Format("You entered: {0}", value);
+	}
+
+	public CompositeType GetDataUsingDataContract(CompositeType composite)
+	{
+		if (composite == null)
+		{
+			throw new ArgumentNullException("composite");
+		}
+		if (composite.BoolValue)
+		{
+			composite.StringValue += "Suffix";
+		}
+		return composite;
+	}
+	Juego juego = new Juego();
+	public string Jugar(List<int> lista)
+    {
+		return "";
+    }
+
+	public List<int> ObtenerNumeros(Random rnd)
+    {
+		//return juego.ObtenerNumeros(rnd);
+    }
+}
+
+public class Juego
+{
+    public int Puntos { get; set; }
+    public int[] Numeros{ get; set; }
+
+	public void SumarPuntos()
+    {
+
+    }
+
+	public  int[] ObtenerNumeros(Random rnd)
+	{
+		int dados = 5;
+
+		int[] numeros = ObtenerArray(rnd, dados);
+
+		Numeros = numeros;
+
+		return numeros;
+	}
+	int[] ObtenerArray(Random rnd, int dados)
+	{
+		int[] numeros = new int[6];
+
+		for (int i = 0; i < dados; i++)
+		{
+			numeros[i] = rnd.Next(1, 7);
+		}
+
+		return numeros;
+	}
+}
